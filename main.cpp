044ce881bbe3cc9d7af944f7755fd244c77bf99e
Cam <cameron.miller294@gmail.com>
2022-02-23 16:22:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 #include <string.h>
 using namespace std;
 
-enum { world, character };
+enum { theWorld, theCharacter };
 
-void prompt();
+int initialPrompt();
 
 class World
 {
 public:
   World();
   ~World();
+  void setLandscape();
+  void setWorldAreas();
+  void getLandscape();
+  void getWorldAreas();
+  void summarize();
 private:
   string landscape;
-  int relativeTime;
-  bool manyWorlds;
+  int worldAreas;
 };
 
 class Character
@@ -22,6 +26,11 @@ class Character
 public:
   Character();
   ~Character();
+  void setName();
+  void set_isHuman();
+  void getName();
+  void get_isHuman();
+  void summarize();
 private:
   string name;
   bool isHuman;
@@ -29,35 +38,126 @@ private:
 
 int main()
 {
-  int startWith;
+  int startWith = initialPrompt();
 
-  prompt();
-  cin >> startWith;
+  if (startWith == theWorld) {
+    World world;
+    Character character;
 
-  if (startWith == world) {
-    World aWorld;
-  } else if (startWith == character) {
-    Character aCharacter;
+    world.summarize();
+    character.summarize();
+  } else if (startWith == theCharacter) {
+    Character character;
+    World world;
+
+    character.summarize();
+    world.summarize();
   }
 
   return 0;
 }
 
-void prompt()
+int initialPrompt()
 {
+  int start;
   cout << "Begin with: (0) World  (1) Character" << "\n";
+  cin >> start;
+  return start;
 }
 
 World::World()
 {
-  cout << "You created a world.";
+  setLandscape();
+  setWorldAreas();
 }
 
 World::~World() {}
 
+void World::setLandscape()
+{
+  int aLandscape;
+  int size = 4;
+  string landscapes[] = {
+    "Flat",
+    "Hilly",
+    "Mountainous",
+    "Platforms"
+  };
+
+  cout << "Landscape is ____" << "\n";
+  for (int i = 0; i < size; i++)
+    cout << i << " " << landscapes[i] << "\n";
+
+  cin >> aLandscape;
+  landscape = landscapes[aLandscape];
+}
+
+void World::setWorldAreas()
+{
+  int numAreas;
+  cout << "How many areas exist in the world? ";
+  cin >> numAreas;
+  worldAreas = numAreas;
+}
+
+void World::getLandscape()
+{
+  cout << "Landscape type: " << landscape << "\n";
+}
+
+void World::getWorldAreas()
+{
+  cout << "Number of areas: " << worldAreas << "\n";
+}
+
+void World::summarize()
+{
+  cout << "World: " << "\n";
+  getLandscape();
+  getWorldAreas();
+  cout << "\n";
+}
+
 Character::Character()
 {
-  cout << "You created a Character.";
+  setName();
+  set_isHuman();
 }
 
 Character::~Character() {}
+
+void Character::setName()
+{
+  string aName;
+  cout << "What is the character's name? ";
+  cin >> aName;
+  name = aName;
+}
+
+void Character::set_isHuman()
+{
+  char characterIsHuman;
+  cout << "Is the character human? (y/n) ";
+  cin >> characterIsHuman;
+  isHuman = characterIsHuman == 'y' ? true : false;
+  cout << "\n";
+}
+
+void Character::getName()
+{
+  cout << "Name of character: " << name << "\n";
+}
+
+void Character::get_isHuman()
+{
+  string output = isHuman ? "true" : "false";
+  cout << "Human character: " << output << "\n";
+}
+
+void Character::summarize()
+{
+  cout << "Character: " << "\n";
+  getName();
+  get_isHuman();
+  cout << "\n";
+}
