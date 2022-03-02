9ef2dca777dde08c52ff6e361f74c89e0f11a7ab
Cam <cameron.miller294@gmail.com>
2022-03-01 22:48:50 -0500

    Added SDL Interface

--- main.cpp
+++ main.cpp
@@ -1,55 +1,77 @@
-#include <iostream>
-#include <string.h>
+#include <SDL2/SDL.h>
+#include "main.hpp"
 using namespace std;
 
-enum { theWorld, theCharacter };
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
 
-int initialPrompt();
-char editPrompt();
+void askQuestion();
 
-class World
+int main(int arc, char *argv[])
 {
-public:
-  World();
-  ~World();
-  void setLandscape();
-  void setWorldAreas();
-  void getLandscape();
-  void getWorldAreas();
-  void summarize();
-private:
-  string landscape;
-  int worldAreas;
-};
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window;
+  SDL_Renderer *renderer;
 
-class Character
-{
-public:
-  Character();
-  ~Character();
-  void setName();
-  void setRole();
-  void set_isHuman();
-  void getName();
-  void getRole();
-  void get_isHuman();
-  void summarize();
-private:
-  string name;
-  string role;
-  bool isHuman;
-};
+  window = SDL_CreateWindow("Game Window",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    0
+  );
 
-int main()
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  int running = 1;
+  SDL_Event event;
+  SDL_Rect rect = { 220, 140, 200, 200 };
+
+  while (running)
+  {
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_QUIT:
+          running = 0;
+          break;
+        case SDL_WINDOWEVENT_CLOSE:
+          running = 0;
+          if (window)
+            window = NULL;
+          break;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
+
+void askQuestions()
 {
   int startWith = initialPrompt();
 
-  if (startWith == theWorld) {
+  if (startWith == theWorld)
+  {
     World world;
     Character character;
     world.summarize();
     character.summarize();
-  } else if (startWith == theCharacter) {
+  }
+  else if (startWith == theCharacter)
+  {
     Character character;
     World world;
     character.summarize();
@@ -57,152 +79,12 @@ int main()
   }
 
   int wantsEdit = editPrompt();
-  if (wantsEdit == 'y') {
-    while (wantsEdit == 'y') {
+  if (wantsEdit == 'y')
+  {
+    while (wantsEdit == 'y')
+    {
       cout << "Continue? (0) no  (1) yes ";
       cin >> wantsEdit;
     }
   }
-
-  return 0;
-}
-
-int initialPrompt()
-{
-  int start;
-  cout << "Begin with: (0) World  (1) Character" << "\n";
-  cin >> start;
-  return start;
-}
-
-char editPrompt()
-{
-  char wantsEdit;
-  cout << "Edit data before saving? (y/n) ";
-  cin >> wantsEdit;
-  return wantsEdit;
-}
-
-World::World()
-{
-  setLandscape();
-  setWorldAreas();
-}
-
-World::~World() {}
-
-void World::setLandscape()
-{
-  int aLandscape;
-  int size = 4;
-  string landscapes[] = {
-    "Flat",
-    "Hilly",
-    "Mountainous",
-    "Platforms"
-  };
-
-  cout << "Landscape is ____" << "\n";
-  for (int i = 0; i < size; i++)
-    cout << i << " " << landscapes[i] << "\n";
-
-  cin >> aLandscape;
-  landscape = landscapes[aLandscape];
-}
-
-void World::setWorldAreas()
-{
-  int numAreas;
-  cout << "How many areas exist in the world? ";
-  cin >> numAreas;
-  worldAreas = numAreas;
-}
-
-void World::getLandscape()
-{
-  cout << "Landscape type: " << landscape << "\n";
-}
-
-void World::getWorldAreas()
-{
-  cout << "Number of areas: " << worldAreas << "\n";
-}
-
-void World::summarize()
-{
-  cout << "World: " << "\n";
-  getLandscape();
-  getWorldAreas();
-  cout << "\n";
-}
-
-Character::Character()
-{
-  setName();
-  setRole();
-  set_isHuman();
-}
-
-Character::~Character() {}
-
-void Character::setName()
-{
-  string aName;
-  cout << "What is the character's name? ";
-  cin >> aName;
-  name = aName;
-}
-
-void Character::setRole()
-{
-  int aRole;
-  int size = 5;
-  string roles[] = {
-    "Protagonist",
-    "Antagonist",
-    "Sidekick",
-    "Confidante",
-    "Extra"
-  };
-
-  cout << "What is the character's role? " << "\n";
-  for (int i = 0; i < size; i++)
-    cout << i << " " << roles[i] << "\n";
-
-  cin >> aRole;
-  role = roles[aRole];
-}
-
-void Character::set_isHuman()
-{
-  char characterIsHuman;
-  cout << "Is the character human? (y/n) ";
-  cin >> characterIsHuman;
-  isHuman = characterIsHuman == 'y' ? true : false;
-  cout << "\n";
-}
-
-void Character::getName()
-{
-  cout << "Name: " << name << "\n";
-}
-
-void Character::getRole()
-{
-  cout << "Role: " << role << "\n";
-}
-
-void Character::get_isHuman()
-{
-  string output = isHuman ? "true" : "false";
-  cout << "Human: " << output << "\n";
-}
-
-void Character::summarize()
-{
-  cout << "Character: " << "\n";
-  getName();
-  getRole();
-  get_isHuman();
-  cout << "\n";
 }
---
+++ main.hpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+enum { theWorld, theCharacter };
+
+int initialPrompt();
+char editPrompt();
+
+class World
+{
+public:
+  World();
+  ~World();
+  void setLandscape();
+  void setWorldAreas();
+  void getLandscape();
+  void getWorldAreas();
+  void summarize();
+private:
+  string landscape;
+  int worldAreas;
+};
+
+class Character
+{
+public:
+  Character();
+  ~Character();
+  void setName();
+  void setRole();
+  void set_isHuman();
+  void getName();
+  void getRole();
+  void get_isHuman();
+  void summarize();
+private:
+  string name;
+  string role;
+  bool isHuman;
+};
+
+int initialPrompt()
+{
+  int start;
+  cout << "Begin with: (0) World  (1) Character" << "\n";
+  cin >> start;
+  return start;
+}
+
+char editPrompt()
+{
+  char wantsEdit;
+  cout << "Edit data before saving? (y/n) ";
+  cin >> wantsEdit;
+  return wantsEdit;
+}
+
+World::World()
+{
+  setLandscape();
+  setWorldAreas();
+}
+
+World::~World() {}
+
+void World::setLandscape()
+{
+  int aLandscape;
+  int size = 4;
+  string landscapes[] = {
+    "Flat",
+    "Hilly",
+    "Mountainous",
+    "Platforms"
+  };
+
+  cout << "Landscape is ____" << "\n";
+  for (int i = 0; i < size; i++)
+    cout << i << " " << landscapes[i] << "\n";
+
+  cin >> aLandscape;
+  landscape = landscapes[aLandscape];
+}
+
+void World::setWorldAreas()
+{
+  int numAreas;
+  cout << "How many areas exist in the world? ";
+  cin >> numAreas;
+  worldAreas = numAreas;
+}
+
+void World::getLandscape()
+{
+  cout << "Landscape type: " << landscape << "\n";
+}
+
+void World::getWorldAreas()
+{
+  cout << "Number of areas: " << worldAreas << "\n";
+}
+
+void World::summarize()
+{
+  cout << "World: " << "\n";
+  getLandscape();
+  getWorldAreas();
+  cout << "\n";
+}
+
+Character::Character()
+{
+  setName();
+  setRole();
+  set_isHuman();
+}
+
+Character::~Character() {}
+
+void Character::setName()
+{
+  string aName;
+  cout << "What is the character's name? ";
+  cin >> aName;
+  name = aName;
+}
+
+void Character::setRole()
+{
+  int aRole;
+  int size = 5;
+  string roles[] = {
+    "Protagonist",
+    "Antagonist",
+    "Sidekick",
+    "Confidante",
+    "Extra"
+  };
+
+  cout << "What is the character's role? " << "\n";
+  for (int i = 0; i < size; i++)
+    cout << i << " " << roles[i] << "\n";
+
+  cin >> aRole;
+  role = roles[aRole];
+}
+
+void Character::set_isHuman()
+{
+  char characterIsHuman;
+  cout << "Is the character human? (y/n) ";
+  cin >> characterIsHuman;
+  isHuman = characterIsHuman == 'y' ? true : false;
+  cout << "\n";
+}
+
+void Character::getName()
+{
+  cout << "Name: " << name << "\n";
+}
+
+void Character::getRole()
+{
+  cout << "Role: " << role << "\n";
+}
+
+void Character::get_isHuman()
+{
+  string output = isHuman ? "true" : "false";
+  cout << "Human: " << output << "\n";
+}
+
+void Character::summarize()
+{
+  cout << "Character: " << "\n";
+  getName();
+  getRole();
+  get_isHuman();
+  cout << "\n";
+}
