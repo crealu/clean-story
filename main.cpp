892ae3013281d639fbe1ce3fdc76d1ea004f900f
Cam <cameron.miller294@gmail.com>
2022-04-03 23:03:06 -0400

    Update main
    Finally got font to display on SDL surface

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
+#include <iostream>
+#include <string>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include "main.hpp"
 #include "entity.cpp"
 using namespace std;
 
+#define IDI_ICON 100
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 480
 
+IDI_ICON ICON "favicon.ico";
 void drawBackground(SDL_Renderer *renderer);
 int quitGame(int running, SDL_Window *window, SDL_Event &event);
 void checkVicinity(Player player, Wizard wizard, SDL_Event &event);
@@ -18,13 +22,6 @@ int main(int arc, char *argv[]) {
   SDL_Renderer *renderer;
   SDL_Event event;
 
-  TTF_Font *font;
-  font = TTF_OpenFont("arial.ttf", 12);
-
-  SDL_Color fontColor = {250, 250, 250};
-  SDL_Surface *text;
-  text = TTF_RenderText_Solid(font, "yo", fontColor);
-
   window = SDL_CreateWindow("Game Window",
     SDL_WINDOWPOS_UNDEFINED,
     SDL_WINDOWPOS_UNDEFINED,
@@ -35,27 +32,59 @@ int main(int arc, char *argv[]) {
 
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+  TTF_Init();
+  TTF_Font *theFont = TTF_OpenFont("Comfortaa[wght].ttf", 24);
+
+  if (!theFont) {
+    printf("TTF_OpenFont: %s\n", TTF_GetError());
+  }
+  SDL_Color fontColor = {255, 255, 255};
+  SDL_Surface *surfaceText = TTF_RenderText_Solid(theFont, "Hello", fontColor);
+  SDL_Texture *textureText = SDL_CreateTextureFromSurface(renderer, surfaceText);
+  SDL_FreeSurface(surfaceText);
+
+  SDL_Rect fontRect;
+  fontRect.x = 10;
+  fontRect.y = 10;
+  fontRect.w = 60;
+  fontRect.h = 30;
+
   int running = 1;
-  Player player(100, 100);
+  Player player(200, 300);
   Wizard wizard(200, 200);
+  Wizard wizard2(200, 250);
 
   while (running) {
     while (SDL_PollEvent(&event)) {
       running = quitGame(running, window, event);
-      player.move(event);
-      checkVicinity(player, wizard, event);
+      // player.move(event);
+      // checkVicinity(player, wizard, event);
+      if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_b) {
+          fontRect.x += 10;
+        }
+        if (event.key.keysym.sym == SDLK_h) {
+          fontRect.w = 0;
+        }
+        if (event.key.keysym.sym == SDLK_g) {
+          fontRect.w = 60;
+        }
+      }
     }
 
-    SDL_SetRenderDrawColor(renderer, 200, 15, 0, 255);
-
     drawBackground(renderer);
+    SDL_RenderCopy(renderer, textureText, NULL, &fontRect);
+
     player.draw(renderer);
     wizard.draw(renderer);
+    wizard2.draw(renderer);
     SDL_RenderPresent(renderer);
   }
 
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
+  SDL_DestroyTexture(textureText);
+  TTF_CloseFont(theFont);
   SDL_Quit();
   return 0;
 }
