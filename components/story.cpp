cd99086c671fb31dc53fda0acf9520f24a37410f
Cam <cameron.miller294@gmail.com>
2022-05-06 12:58:46 -0400

    Update story.cpp
    Created testable logic using input and output to follow display of const char * array contents

--- components/story.cpp
+++ components/story.cpp
@@ -1,23 +1,61 @@
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <stdio.h>
+// #include <SDL2/SDL.h>
 using namespace std;
 
-class StoryScript {
-public:
-  StoryScript();
-  ~StoryScript();
+// class StoryScript {
+// public:
+//   StoryScript();
+//   ~StoryScript();
+//
+//   void writeText();
+//   void setCurrent(char c);
+//
+// protected:
+//   char *script;
+//   int currentLine;
+// };
+//
+// StoryScript::StoryScript() {
+//   const char *text[] = {
+//     "Serene voyager unbound by perpetuated chasm, the trial of times forgotten can only one bear.",
+//     "So... what brings you to Midoritierra?",
+//     "...",
+//     "...",
+//     "Excuse my unmannerliness... for I have yet to introduce myself.",
+//     "I am the sourcerer of these plains,",
+//     "maintainer of peace and order,",
+//     "caster of the breeze of tranquility,"
+//     "wielder of the inifinte grön magic.",
+//     "And my friends call me G."
+//   };
+//
+//   script = new char[sizeof text];
+//   currentLine = 0;
+//
+//   for (int t = 0; t < sizeof text; t++)
+//     script[t] = text[t];
+// }
+// StoryScript::~StoryScript() {}
+//
+// void StoryScript::writeText() {
+//   cout << script[currentLine] << "\n";
+// }
+//
+// void StoryScript::setCurrent(char c) {
+//   if (c == 'n' && currentLine != 9)
+//     currentLine += 1;
+//
+//   if (c == 'b' && currentLine != 0)
+//     currentLine -= 1;
+// }
 
-  void writeText();
+int main() {
+  // StoryScript green;
+  char c;
+  int curr = 0;
 
-protected:
-  char *script;
-};
-
-StoryScript::StoryScript() {}
-StoryScript::~StoryScript() {}
-
-void StoryScript::writeText() {
-  char text = {
+  const char *text[] = {
     "Serene voyager unbound by perpetuated chasm, the trial of times forgotten can only one bear.",
     "So... what brings you to Midoritierra?",
     "...",
@@ -28,10 +66,21 @@ void StoryScript::writeText() {
     "caster of the breeze of tranquility,"
     "wielder of the inifinte grön magic.",
     "And my friends call me G."
+  };
+
+  while (c != 'e') {
+    cout << "n/b ? ";
+    cin >> c;
+    if (c == 'n')
+      curr += 1;
+
+    if (c == 'b')
+      curr -= 1;
+
+    cout << "\n" << text[curr] << "\n";
+    // green.writeText();
+    // green.setCurrent(c);
   }
 
-  script = new char[text.size()];
-
-  for (int t = 0; t < text.size(); t++)
-    script[t] = text[t];
+  return 0;
 }
