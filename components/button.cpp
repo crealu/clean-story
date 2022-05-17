d81625560083af5d03f97f61723fca171ef85bfd
Cam <cameron.miller294@gmail.com>
2022-05-16 23:16:26 -0400

    Create button.cpp
    Button will use the Circle class to create a UI text button

---
+++ components/button.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <math.h>
+
+class Button {
+public:
+  Button();
+  ~Button();
+  void draw(SDL_Renderer *renderer);
+
+protected:
+  int p;
+  int xp;
+  int *xPoints;
+  int *yPoints;
+  int cx;
+  int cy;
+  int pLen;
+};
+
+Button::Button() {
+  float pn = 6.29;
+  float pInc = 0.09;
+  int p;
+  pLen = pn / pInc;
+
+  cx = 320;
+  cy = 400;
+
+  xPoints = new int[pLen];
+  yPoints = new int[pLen];
+
+  float r = 0.0;
+  for (r = 0.0, p = 0; r <= pn; r += pInc, p++)
+    if (p < pLen)
+      xPoints[p] = round( 20 * (cos(r/1.0)) );
+
+  for (r = 0.0, p = 0; r <= pn; r += pInc, p++)
+    if (p < pLen)
+      yPoints[p] = round( 20 * (sin(r/1.0)) );
+};
+
+Button::~Button() {}
+
+void Button::draw(SDL_Renderer *renderer) {
+  for (xp = 0; xp < pLen; xp++)
+    SDL_RenderDrawPoint(renderer, cx+xPoints[xp], cy+yPoints[xp]);
+}
