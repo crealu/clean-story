5487a397f7d3c4d1bd6b2fe239ace8037ca87804
Cam <cameron.miller294@gmail.com>
2022-05-22 23:54:45 -0400

    Update music.cpp

--- components/music.cpp
+++ components/music.cpp
@@ -3,7 +3,19 @@
 class Music {
   Music();
   ~Music();
+  void play();
+  Mix_Music *music;
 };
 
-Music::Music() {}
+Music::Music() {
+  Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 1024);
+  Mix_AllocateChannels(16);
+  Mix_Volume(1, MIX_MAX_VOLUME/2);
+  music = Mix_LoadMUS("assets/audio/phased.mp3");
+
+}
 Music::~Music() {}
+
+void Music::play() {
+  Mix_PlayMusic(music, -1);
+}
