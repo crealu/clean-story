928b8755b3f3796a1ea6b8b17133ee336048d661
Cam <cameron.miller294@gmail.com>
2022-05-27 10:44:38 -0400

    Update dialog.cpp
    Text rendered more clearly and proportionally

--- ui/dialog.cpp
+++ ui/dialog.cpp
@@ -8,31 +8,33 @@ public:
   int getLength(const char *text);
 
 protected:
-  SDL_Rect textRect;
-  SDL_Texture *textureText;
+  SDL_Rect rect;
+  SDL_Texture *texture;
   int xPos;
   int yPos;
 };
 
-void Dialog::setDialog(TTF_Font *aFont, SDL_Renderer *renderer, const char *theText) {
+void Dialog::setDialog(TTF_Font *font, SDL_Renderer *renderer, const char *text) {
   SDL_Color fontColor = {0, 0, 0};
-  SDL_Surface *surfaceText = TTF_RenderText_Solid(aFont, theText, fontColor);
-  textureText = SDL_CreateTextureFromSurface(renderer, surfaceText);
-  textRect.x = 20;
-  textRect.y = 400;
-  textRect.h = 30;
-  if (15 * getLength(theText) >= 600) {
-    textRect.w = 10 * getLength(theText);
-  } else {
-    textRect.w = 15 * getLength(theText);
-  }
-  SDL_FreeSurface(surfaceText);
+  SDL_Surface *surface = TTF_RenderText_Blended(font, text, fontColor);
+  texture = SDL_CreateTextureFromSurface(renderer, surface);
+  rect.x = 0;
+  rect.y = 450;
+  rect.h = 30;
+  rect.w = 12 * getLength(text);
+  // if (15 * getLength(theText) >= 600) {
+  //   textRect.w = 10 * getLength(theText);
+  // } else {
+  //   textRect.w = 15 * getLength(theText);
+  // }
+  SDL_FreeSurface(surface);
 }
 
 void Dialog::draw(SDL_Renderer *renderer) {
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderFillRect(renderer, &textRect);
-  SDL_RenderCopy(renderer, textureText, NULL, &textRect);
+  SDL_RenderFillRect(renderer, &rect);
+  SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
+  SDL_RenderCopy(renderer, texture, NULL, &rect);
 }
 
 int Dialog::getLength(const char *text) {
