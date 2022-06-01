00b3aa329d6f4caeae5984bfb8542e9e3efcec50
Cam <cameron.miller294@gmail.com>
2022-06-01 14:39:04 -0400

    Update button.cpp
    Added a fill color to the button and changed its location

--- ui/button.cpp
+++ ui/button.cpp
@@ -11,69 +11,34 @@ public:
   void setText(TTF_Font *font, SDL_Renderer *renderer);
 
 protected:
-  int p;
-  int xp;
-  int *xPoints;
-  int *yPoints;
-  int cx;
-  int cy;
-  int xy;
-  int fx;
-  int fy;
-  int pLen;
-  float pi;
+  int x;
+  int y;
+  int radius;
   SDL_Texture *texture;
   SDL_Rect rect;
-  SDL_Vertex vertices[];
 };
 
 Button::Button() {
-  float pInc = 0.09;
-  int p;
-  pi = 6.29;
-  pLen = pi / pInc;
-  cx = 40;
-  cy = 460;
-  fx = 0;
-  fy = 0;
-
-  xPoints = new int[pLen];
-  yPoints = new int[pLen];
-
-  float r = 0.0;
-  for (r = 0.0, p = 0; r <= pi; r += pInc, p++)
-    if (p < pLen)
-      xPoints[p] = round( 20 * (cos(r/1.0)) );
-
-  for (r = 0.0, p = 0; r <= pi; r += pInc, p++)
-    if (p < pLen)
-      yPoints[p] = round( 20 * (sin(r/1.0)) );
-  // 
-  // int v;
-  // for (v = 0; v < sizeof(xPoints)/sizeof(xPoints[0]); v++) {
-  //   vertices[v].position.x = xPoints[v];
-  //   vertices[v].position.y = yPoints[v];
-  // }
-
-  xy = 0;
+  x = 605;
+  y = 390;
+  radius = 16;
 }
 
 Button::~Button() {}
 
 void Button::draw(SDL_Renderer *renderer) {
-  fx = 0;
-  fy = 0;
-  for (xp = 0; xp < pLen; xp++)
-    SDL_RenderDrawPoint(renderer, cx+xPoints[xp], cy+yPoints[xp]);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  for (int w = 0; w < radius * 2; w++) {
+    for (int h = 0; h < radius * 2; h++) {
+      int dx = radius - w; // horizontal offset
+      int dy = radius - h; // vertical offset
+      if ((dx * dx + dy * dy) <= (radius * radius)) {
+        SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+      }
+    }
+  }
 
-  // for (xy = 0; xy < 360; xy += 0.1) {
-  //   fx = round(20 * cos(xy * pi / 180));
-  //   fy = round(20 * sin(xy * pi / 180));
-  //   SDL_RenderDrawPoint(renderer, cx+fx, cx+fy);
-  // }
-
-  // SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
-
+  SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
   SDL_RenderCopy(renderer, texture, NULL, &rect);
 }
 
@@ -81,9 +46,39 @@ void Button::setText(TTF_Font *font, SDL_Renderer *renderer) {
   SDL_Color fontColor = {255, 255, 255};
   SDL_Surface *surface = TTF_RenderText_Blended(font, "M", fontColor);
   texture = SDL_CreateTextureFromSurface(renderer, surface);
-  rect.x = 305;
-  rect.y = 450;
-  rect.h = 20;
-  rect.w = 20;
+  rect.x = x - 9;
+  rect.y = y - 9;
+  rect.h = 14;
+  rect.w = 14;
   SDL_FreeSurface(surface);
 }
+
+// void Button::drawCircle(SDL_Renderer *renderer, int x, int y, int radius) {
+//   SDL_SetRenderDrawColor(renderer, 155, 155, 155, 155);
+//   for (int w = 0; w < radius * 2; w++) {
+//     for (int h = 0; h < radius * 2; h++) {
+//       int dx = radius - w; // horizontal offset
+//       int dy = radius - h; // vertical offset
+//       if ((dx * dx + dy * dy) <= (radius * radius)) {
+//         SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+//       }
+//     }
+//   }
+// }
+//
+// void draw_circle(SDL_Renderer *renderer, int x, int y, int radius, SDL_Color color)
+// {
+//   SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+//   for (int w = 0; w < radius * 2; w++)
+//   {
+//       for (int h = 0; h < radius * 2; h++)
+//       {
+//           int dx = radius - w; // horizontal offset
+//           int dy = radius - h; // vertical offset
+//           if ((dx*dx + dy*dy) <= (radius * radius))
+//           {
+//               SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+//           }
+//       }
+//   }
+// }
