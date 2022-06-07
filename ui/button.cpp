12398a70d7c72f0560ff4268b44196369b43b0e7
Cam <cameron.miller294@gmail.com>
2022-06-07 13:04:29 -0400

    Update button.cpp
    Created clearer button design

--- ui/button.cpp
+++ ui/button.cpp
@@ -8,25 +8,42 @@ public:
   Button();
   ~Button();
   void draw(SDL_Renderer *renderer);
+  void drawContainer(SDL_Renderer *renderer);
+  void drawCircle(SDL_Renderer *renderer);
+  void drawText(SDL_Renderer *renderer);
   void setText(TTF_Font *font, SDL_Renderer *renderer);
 
 private:
   int x;
   int y;
   int radius;
-  SDL_Texture *texture;
-  SDL_Rect rect;
+  SDL_Texture *circleTexture;
+  SDL_Texture *textTexture;
+  SDL_Surface *circleSurface;
+  SDL_Surface *textSurface;
+  SDL_Rect containerRect;
+  SDL_Rect circleRect;
+  SDL_Rect textRect;
 };
 
 Button::Button() {
-  x = 605;
+  x = 300;
   y = 390;
   radius = 16;
+  int offset = 20;
+  SDL_Rect cRect = {x - offset, y - offset, 90, 40};
+  containerRect = cRect;
 }
 
 Button::~Button() {}
 
 void Button::draw(SDL_Renderer *renderer) {
+  drawContainer(renderer);
+  drawCircle(renderer);
+  drawText(renderer);
+}
+
+void Button::drawCircle(SDL_Renderer *renderer) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   for (int w = 0; w < radius * 2; w++) {
     for (int h = 0; h < radius * 2; h++) {
@@ -37,18 +54,36 @@ void Button::draw(SDL_Renderer *renderer) {
       }
     }
   }
+  SDL_QueryTexture(circleTexture, NULL, NULL, &circleRect.w, &circleRect.h);
+  SDL_RenderCopy(renderer, circleTexture, NULL, &circleRect);
+}
 
-  SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
-  SDL_RenderCopy(renderer, texture, NULL, &rect);
+void Button::drawContainer(SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderFillRect(renderer, &containerRect);
+}
+
+void Button::drawText(SDL_Renderer *renderer) {
+  SDL_QueryTexture(textTexture, NULL, NULL, &textRect.w, &textRect.h);
+  SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
 }
 
 void Button::setText(TTF_Font *font, SDL_Renderer *renderer) {
-  SDL_Color fontColor = {255, 255, 255};
-  SDL_Surface *surface = TTF_RenderText_Blended(font, "M", fontColor);
-  texture = SDL_CreateTextureFromSurface(renderer, surface);
-  rect.x = x - 9;
-  rect.y = y - 9;
-  rect.h = 14;
-  rect.w = 14;
-  SDL_FreeSurface(surface);
+  SDL_Color circleFontColor = {255, 255, 255};
+  circleSurface = TTF_RenderText_Blended(font, "M", circleFontColor);
+  circleTexture = SDL_CreateTextureFromSurface(renderer, circleSurface);
+  circleRect.x = x - 9;
+  circleRect.y = y - 9;
+  circleRect.h = 14;
+  circleRect.w = 14;
+  SDL_FreeSurface(circleSurface);
+
+  SDL_Color textFontColor = {0, 0, 0};
+  textSurface = TTF_RenderText_Blended(font, "Talk", textFontColor);
+  textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+  textRect.x = x + 20;
+  textRect.y = y -10;
+  textRect.h = 30;
+  textRect.w = 20;
+  SDL_FreeSurface(textSurface);
 }
